fabaf9548aba299b39d86057077d0a6341c5083d
avalcarcel9 <alval@pennmedicine.upenn.ed>
2018-04-20 22:23:08 -0400

    adding my flow functions

--- README.md
+++
@@ -1,2 +0,0 @@
-# RFunctions
-Example functions adapted from https://github.com/muschellij2/muschflow. These can be used as an example for going from R functions to an R package.
---
+++ printroxygenheader.R
@@ -0,0 +1,15 @@
+printroxygenheader <- function(){
+  message("
+          #' @title <Brief Description>
+          #' @description <Full Description>
+          #' @param input_parameter <Input Parameter Description>
+          #' @export <Do you want users to be able to use this function? If not, it is a hidden function.>
+          #' @keywords
+          #' @seealso
+          #' @import <Load entire package as dependency>
+          #' @importFrom <Load specific functions from packages as dependencies>
+          #' @return Object returned
+          #' @aliases
+          #' @examples
+          #'")
+}
